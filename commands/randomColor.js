a3e552395b9bd5df965db7260a0e946379fab6ff
Striker <zzstrikerzzyt@gmail.com>
2020-07-16 15:30:32 +0100

    Added randomColor command

---
+++ commands/randomColor.js
@@ -0,0 +1,32 @@
+const { Command } = require('discord-akairo');
+
+function sleep(ms) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    });
+}
+
+class RandomColorCommand extends Command {
+    constructor() {
+        super('randomColor', {
+            category: 'general',
+            aliases: ['randomColor', 'rc'],
+            typing: true,
+        });
+    }
+
+    async exec(msg) {
+        const randomColor = Math.floor(Math.random() * 16777215).toString(16)
+        const randomColorEmbed = this.client.util.embed()
+            .setTimestamp()
+            .setTitle(`Color: ${randomColor}`)
+            .setDescription('This message will delete in 15 seconds, so if you like this color save it.')
+            .setColor(randomColor);
+        const message = await msg.reply(randomColorEmbed);
+        await sleep(15000);
+        msg.delete();
+        message.delete();
+    }
+}
+
+module.exports = RandomColorCommand;
