19b10532ee81a05f4e1462c48729d89366c49e8a
Striker <zzstrikerzzyt@gmail.com>
2020-07-16 15:30:06 +0100

    Removed useless if statements

--- commands/colorRequest.js
+++ commands/colorRequest.js
@@ -24,31 +24,13 @@ class ColorRequestCommand extends Command {
     async exec(msg, args) {
         if (msg.member.roles.highest.name.includes('color') == true) {
             const errorMessage = await msg.reply('You already have a color role and cannot request another.');
-              await sleep(10000);
-              errorMessage.delete();
-              msg.delete();
-              return;
-          }
+            await sleep(10000);
+            errorMessage.delete();
+            msg.delete();
+            return;
+        }
         if (!args.hex) {
             const errorMessage = msg.reply('Please rerun the command with a valid hex code.');
-            await sleep(10000);
-            errorMessage.delete();
-            msg.delete();
-            return;
-        }
-        // Safeguard for if person already has a color role. Will update role name if their username is different.
-        if (msg.member.roles.highest.name.substring(6) != msg.author.username.toLowerCase() && msg.member.roles.highest.name != '@everyone' && msg.member.roles.highest.name != 'c sisters' && msg.member.roles.highest.name != 'OGs') {
-            msg.member.roles.highest.edit({ name: `color ${msg.author.username.toLowerCase()}` });
-
-            const errorMessage = await msg.reply('You already have a color role and cannot request another.\nI have updated your role name as your username did not match with the role.');
-            await sleep(10000);
-            errorMessage.delete();
-            msg.delete();
-            return;
-        }
-
-        if (!args.hex.toString().includes('#')) {
-            const errorMessage = await msg.reply('Your message does not fit the format.\nPlease rerun the command with a "#" at the beginning of the Hex Code.');
             await sleep(10000);
             errorMessage.delete();
             msg.delete();
@@ -64,7 +46,7 @@ class ColorRequestCommand extends Command {
             role.setPosition((msg.member.roles.highest.position));
 
             // Role addition
-            const additionPromise = await msg.member.roles.add(role);
+            await msg.member.roles.add(role);
             const successMessage = await msg.reply(`I have successfully given you a role with the color **${args.hex}**`);
             await sleep(10000);
             successMessage.delete();
