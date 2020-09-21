de80a9858b61bdb93648e5d36cdc9e8f10189e7a
Striker <zzstrikerzzyt@gmail.com>
2020-09-21 21:01:37 +0100

    Reverted gayass changes

--- commands/colorRequest.js
+++ commands/colorRequest.js
@@ -1,5 +1,4 @@
 const { Command } = require('discord-akairo');
-const { createRole, addRole } = require('../src/functions.js');
 
 function sleep(ms) {
     return new Promise((resolve) => {
@@ -29,41 +28,6 @@ class ColorRequestCommand extends Command {
             errorMessage.delete();
             msg.delete();
             return;
-        } else if (msg.member.roles.highest.name.includes('Booster') == true) {
-            const filter = (reaction, user) => {
-                return ['👍'].includes(reaction.emoji.name) && user.id === msg.author.id;
-            };
-
-            const confirmationMsg = await msg.channel.send('You already have a booster role, if you would like to replace this with your color role, click the reaction below.');
-            confirmationMsg.react('👍');
-
-            confirmationMsg.awaitReactions(filter, { max: 1, time: 60000, errors: ['time'] })
-                .then(async (collected) => {
-                    const reaction = collected.first();
-
-                    if (reaction.emoji.name === '👍') {
-                        msg.reply('confirmed.');
-
-                        if (!args.hex) {
-                            const errorMessage = msg.reply('Please rerun the command with a valid hex code.');
-                            await sleep(10000);
-                            errorMessage.delete();
-                            msg.delete();
-                            return;
-                        } else {
-                            // Role Creation
-                            const role = await createRole(msg, args);
-
-                            // Role addition
-                            addRole(msg, role, args);
-                            return;
-                        }
-                    }
-                })
-                .catch(collected => {
-                    msg.reply('You didn\'t vote within 1 minute. Please run the command again.');
-                    return;
-                });
         }
         if (!args.hex) {
             const errorMessage = msg.reply('Please rerun the command with a valid hex code.');
@@ -73,10 +37,21 @@ class ColorRequestCommand extends Command {
             return;
         } else {
             // Role Creation
-            const role = await createRole(msg, args);
+            const role = await msg.guild.roles.create({
+                data: {
+                    name: `color ${msg.author.username.toLowerCase()}`,
+                    color: `${args.hex}`
+                }
+            });
+            role.setPosition((msg.member.roles.highest.position));
 
             // Role addition
-            addRole(msg, role, args);
+            await msg.member.roles.add(role);
+            const successMessage = await msg.reply(`I have successfully given you a role with the color **${args.hex}**`);
+            await sleep(10000);
+            successMessage.delete();
+            msg.delete();
+            return;
         }
     }
 }
