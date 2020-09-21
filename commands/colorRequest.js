d425e1501fdd72582b43b61c860053afb7acf7e9
Striker <zzstrikerzzyt@gmail.com>
2020-09-21 20:31:33 +0100

    Fixed issue where booster role messed up logic

--- commands/colorRequest.js
+++ commands/colorRequest.js
@@ -28,6 +28,26 @@ class ColorRequestCommand extends Command {
             errorMessage.delete();
             msg.delete();
             return;
+        } else if (msg.member.roles.highest.name.includes('Booster') == true) {
+            const filter = (reaction, user) => {
+                return ['👍'].includes(reaction.emoji.name) && user.id === msg.author.id;
+            };
+
+            const confirmationMsg = await msg.channel.send('You already have a booster role, if you would like to replace this with your color role, click the reaction below.');
+            confirmationMsg.react('👍');
+
+            confirmationMsg.awaitReactions(filter, { max: 1, time: 60000, errors: ['time'] })
+                .then(collected => {
+                    const reaction = collected.first();
+
+                    if (reaction.emoji.name === '👍') {
+                        msg.reply('confirmed.');
+                    }
+                })
+                .catch(collected => {
+                    msg.reply('You didn\'t vote within 1 minute. Please run the command again.');
+                    return;
+                });
         }
         if (!args.hex) {
             const errorMessage = msg.reply('Please rerun the command with a valid hex code.');
