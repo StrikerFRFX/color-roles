67eabc9b807eee59bd35442ebca07be9ab92f76b
Striker <zzstrikerzzyt@gmail.com>
2020-10-23 15:43:32 +0100

    Added command logging

--- events/message.js
+++ events/message.js
@@ -11,6 +11,19 @@ class messageListener extends Listener {
     exec(msg) {
         if (msg.content.startsWith(this.client.commandHandler.prefix)) {
             if (msg.author.bot) return;
+            this.client.commandHandler.aliases.keyArray().forEach((c) => {
+                if (msg.content.includes(c) && msg.content.startsWith(this.client.commandHandler.prefix)) {
+                    this.client.channels.fetch('768189873126637628').then((channel) => {
+                        const logEmbed = this.client.util.embed()
+                            .setAuthor(msg.author.tag)
+                            .setColor('#00ff00')
+                            .setTimestamp()
+                            .addField(`Command used in ${msg.guild.name}`, msg.content)
+                            .setThumbnail(msg.author.displayAvatarURL({ size: 64 }));
+                        channel.send(logEmbed);
+                    });
+                }
+            });
         }
     }
 }
