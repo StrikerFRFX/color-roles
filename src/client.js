a3e4640049e531c55e331708214ca52fb256344a
Striker <34070194+StrikerFRFX@users.noreply.github.com>
2022-01-05 21:16:12 +0000

    Update client.js

--- src/client.js
+++ src/client.js
@@ -1,48 +1,61 @@
-const { AkairoClient, CommandHandler, ListenerHandler } = require('discord-akairo');
-const { Intents } = require('discord.js');
+const {
+  AkairoClient,
+  CommandHandler,
+  ListenerHandler,
+} = require("discord-akairo");
+const { Intents } = require("discord.js");
 const myIntents = new Intents();
-myIntents.add('DIRECT_MESSAGES', 'DIRECT_MESSAGE_TYPING', 'GUILDS', 'GUILD_EMOJIS', 'GUILD_MEMBERS', 'GUILD_MESSAGES', 'GUILD_MESSAGE_TYPING', 'GUILD_PRESENCES');
+myIntents.add(
+  "DIRECT_MESSAGES",
+  "DIRECT_MESSAGE_TYPING",
+  "GUILDS",
+  "GUILD_EMOJIS_AND_STICKERS",
+  "GUILD_MEMBERS",
+  "GUILD_MESSAGES",
+  "GUILD_MESSAGE_TYPING",
+  "GUILD_PRESENCES"
+);
 
 class Client extends AkairoClient {
-    constructor() {
-        super({
-            intents: myIntents,
-            ownerID: ['215509157837537280'],
-            automateCategories: true
-        });
+  constructor() {
+    super({
+      intents: myIntents,
+      ownerID: ["215509157837537280"],
+      automateCategories: true,
+    });
 
-        this.commandHandler = new CommandHandler(this, {
-            directory: './commands',
-            prefix: '.',
-            allowMention: true,
-            commandUtil: true,
-            storeMessages: true,
-            handleEdits: true,
-        });
+    this.commandHandler = new CommandHandler(this, {
+      directory: "./commands",
+      prefix: ".",
+      allowMention: true,
+      commandUtil: true,
+      storeMessages: true,
+      handleEdits: true,
+    });
 
-        this.listenerHandler = new ListenerHandler(this, {
-            directory: './events'
-        });
+    this.listenerHandler = new ListenerHandler(this, {
+      directory: "./events",
+    });
 
-        this.setup();
-    }
+    this.setup();
+  }
 
-    setup() {
-        this.commandHandler.useListenerHandler(this.listenerHandler);
+  setup() {
+    this.commandHandler.useListenerHandler(this.listenerHandler);
 
-        this.listenerHandler.setEmitters({
-            commandHandler: this.commandHandler,
-            listenerHandler: this.listenerHandler
-        });
+    this.listenerHandler.setEmitters({
+      commandHandler: this.commandHandler,
+      listenerHandler: this.listenerHandler,
+    });
 
-        this.commandHandler.loadAll();
-        this.listenerHandler.loadAll();
-    }
+    this.commandHandler.loadAll();
+    this.listenerHandler.loadAll();
+  }
 
-    async start(token) {
-        await this.login(token);
-        console.log('Ready!'); // eslint-disable-line no-console
-    }
+  async start(token) {
+    await this.login(token);
+    console.log("Ready!"); // eslint-disable-line no-console
+  }
 }
 
 module.exports = Client;
