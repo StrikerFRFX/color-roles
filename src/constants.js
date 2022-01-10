b0e61db6cd34d02352262fefceeb5dc8f09c7059
Striker <34070194+StrikerFRFX@users.noreply.github.com>
2022-01-10 00:52:22 +0000

    refactor: removed old src files

--- src/constants.js
+++
@@ -1,4 +0,0 @@
-const boostNotifyText = 'You have boosted a server which your highest role is your Color Role.\n\nIf you wish to change this, please run the command ".br" in this server to replace your new Booster role with your Color Role.\n\nIf you believe this has been sent in error, please contact Striker#1337 with the details of this DM.\n\nOtherwise, ignore this DM if you do not mind the booster role (you will not be able to use the color changing commands while your booster role is the top role)';
-
-
-module.exports = { boostNotifyText };
